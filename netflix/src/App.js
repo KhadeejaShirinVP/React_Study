34e0fe7bf9ba13cedffd4005dd2b3d448518c742
KhadeejaShirinVP <khadeejasvp@moback.com>
2023-02-23 00:18:01 +0530

    Netflix_Clone

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import NavBar from "./Components/NavBar/NavBar";
+import { action, comedy, documentary, horror, originals, romantic } from './urls';
 import './App.css'
 import Banner from "./Components/Banner/Banner";
+import RowPost from "./Components/RowPost/RowPost";
 
 function App() {
   return (
    <div className="App">
       <NavBar/>
       <Banner/>
+      <RowPost url={originals} title='Netflix Originals'/>
+      <RowPost url={action} title='Action' isSmall/>
+      <RowPost url={comedy} title='Comedy' isSmall/>
+      <RowPost url={horror} title='Horror' isSmall/>
+      <RowPost url={romantic} title='Romantic' isSmall/>
+      <RowPost url={documentary} title='Documentaries' isSmall/>
    </div>
   );
 }
---
+++ netflix/src/Constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl='https://api.themoviedb.org/3';
+export const API_KEY='67c89b07d7213fda91b7700d407ed31e';
+export const imageUrl='https://image.tmdb.org/t/p/original';
